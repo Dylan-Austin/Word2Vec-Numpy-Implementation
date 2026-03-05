bbb5575aad149e59445dd99926a65e0904215620
Dylan-Austin <dylanaustinrhys@gmail.com>
2026-03-05 21:08:26 +0000

    edited forward pass to dot product (negative sampling)

--- word2vec.py
+++ word2vec.py
@@ -3,21 +3,20 @@ import numpy as np
 
 class Word2Vec():
     def __init__(self, vocabulary_size : int, d_embedding : int) -> None:
-        # embedding matrix
-        self.embeddings = np.random.rand(vocabulary_size, d_embedding)
-
-        # linear layer
-        self.weights = np.random.rand(d_embedding, vocabulary_size)
-        self.biases = np.random.rand(vocabulary_size)
+        self.embeddings = np.random.randn(vocabulary_size, d_embedding)
     
+    # takes shape (batch, 2) -> (batch, 1)
+    # expects index of word in vocab
     def forward(self, x : ndarray) -> ndarray:
-        # extract the word embeddings
-        x = self.embeddings[x]
+        x = self.embeddings[x] # (batch, 2, d_embedding)
 
-        # average them
-        x = x.mean(axis=-2)
-
-        # apply the linear layer
-        x = x @ self.weights + self.biases
+        # take dot product of target and context vectors
+        x = np.sum(x[..., 0, :] * x[..., 1, :], axis=-1, keepdims=True) # (batch, 1)
 
         return x
+    
+if __name__ == "__main__":
+    w2v = Word2Vec(1000, 300)
+    
+    output = w2v.forward(np.array([[2, 3], [1, 2]]))
+    print(output.shape)
