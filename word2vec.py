0a4c820923ad909593e4c26d457945bff94b0beb
Dylan-Austin <dylanaustinrhys@gmail.com>
2026-03-07 14:27:37 +0000

    SGD backprop

--- word2vec.py
+++ word2vec.py
@@ -1,40 +1,59 @@
 from numpy import ndarray
 import numpy as np
 
+def sigmoid(x: ndarray) -> ndarray:
+    return np.reciprocal(np.logaddexp(0, -x))
+
 class SG_Word2Vec(): # Skip gram
-    def __init__(self, vocabulary_size : int, d_embedding : int) -> None:
+    def __init__(self, vocabulary_size: int, d_embedding: int) -> None:
         self.input_embeddings = np.random.randn(vocabulary_size, d_embedding)
         self.output_embeddings = np.random.randn(vocabulary_size, d_embedding)
     
     # center_word : (batch, 1), positive_context : (batch, 1), negative_samples : (batch, neg_samples)
-    def forward(self, center_word : ndarray, positive_context : ndarray, negative_samples : ndarray) -> ndarray:
+    def training_step(self, center_word: ndarray, positive_context: ndarray, negative_samples: ndarray, lr: float) -> ndarray:
         center_embedding = self.input_embeddings[center_word]
         
         positive_embedding = self.output_embeddings[positive_context]   
-        negative_embedding = self.output_embeddings[negative_samples]
+        negative_embeddings = self.output_embeddings[negative_samples]
 
-        embeddings = np.concatenate((positive_embedding, negative_embedding),axis=-2)
+        embeddings = np.concatenate((positive_embedding, negative_embeddings),axis=-2)
 
         logits = np.sum(center_embedding * embeddings, axis=-1)
-
 
         positive_logit = logits[:, 0]
         negative_logits = logits[:, 1:]
 
+        # backprop step
         positive_loss = np.logaddexp(0, -positive_logit)
         negative_loss = np.sum(np.logaddexp(0, negative_logits), axis=-1)
         
+        positive_loss_gradient = (sigmoid(positive_logit) - 1).reshape(-1, 1, 1)
+        negative_loss_gradient = np.expand_dims(sigmoid(negative_logits), -1)
+
+        # positive loss gradients
+        center_gradients = positive_loss_gradient * positive_embedding
+        # negative loss gradients
+        center_gradients += np.sum(negative_loss_gradient * negative_embeddings, axis=-2, keepdims=True)
+
+        positive_gradients = positive_loss_gradient * center_embedding
+        negative_gradients = negative_loss_gradient * center_embedding
+
+        # SGD
+        np.add.at(self.input_embeddings, center_word, -lr * center_gradients)
+        np.add.at(self.output_embeddings, positive_context, -lr * positive_gradients)
+        np.add.at(self.output_embeddings, negative_samples, -lr * negative_gradients)
+
         batch_loss = positive_loss + negative_loss
-        loss = np.sum(batch_loss)
+        loss = np.mean(batch_loss)
 
         return loss
 
 if __name__ == "__main__":
     w2v = SG_Word2Vec(1000, 300)
 
-    center_word = np.array([[2], [5], [3], [2]])
+    center_word = np.array([[2], [5], [5], [2]])
     positive_context = np.array([[7], [43], [12], [4]])
     negative_samples = np.array([[5, 3, 2], [5, 6, 7], [4, 3, 2], [9, 8, 1]])
 
-    loss = w2v.forward(center_word, positive_context, negative_samples)
+    loss = w2v.training_step(center_word, positive_context, negative_samples, 0.01)
     print(loss)
