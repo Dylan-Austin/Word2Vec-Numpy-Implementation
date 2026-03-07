9d229ddb3b2f00aa70b789afc0bef41f553b23d6
Dylan-Austin <dylanaustinrhys@gmail.com>
2026-03-07 13:21:38 +0000

    corrected forward pass with two embeddings and added BCE

--- word2vec.py
+++ word2vec.py
@@ -1,22 +1,40 @@
 from numpy import ndarray
 import numpy as np
 
-class Word2Vec():
+class SG_Word2Vec(): # Skip gram
     def __init__(self, vocabulary_size : int, d_embedding : int) -> None:
-        self.embeddings = np.random.randn(vocabulary_size, d_embedding)
+        self.input_embeddings = np.random.randn(vocabulary_size, d_embedding)
+        self.output_embeddings = np.random.randn(vocabulary_size, d_embedding)
     
-    # takes shape (batch, 2) -> (batch, 1)
-    # expects index of word in vocab
-    def forward(self, x : ndarray) -> ndarray:
-        x = self.embeddings[x] # (batch, 2, d_embedding)
+    # center_word : (batch, 1), positive_context : (batch, 1), negative_samples : (batch, neg_samples)
+    def forward(self, center_word : ndarray, positive_context : ndarray, negative_samples : ndarray) -> ndarray:
+        center_embedding = self.input_embeddings[center_word]
+        
+        positive_embedding = self.output_embeddings[positive_context]   
+        negative_embedding = self.output_embeddings[negative_samples]
 
-        # take dot product of target and context vectors
-        x = np.sum(x[..., 0, :] * x[..., 1, :], axis=-1, keepdims=True) # (batch, 1)
+        embeddings = np.concatenate((positive_embedding, negative_embedding),axis=-2)
 
-        return x
-    
+        logits = np.sum(center_embedding * embeddings, axis=-1)
+
+
+        positive_logit = logits[:, 0]
+        negative_logits = logits[:, 1:]
+
+        positive_loss = np.logaddexp(0, -positive_logit)
+        negative_loss = np.sum(np.logaddexp(0, negative_logits), axis=-1)
+        
+        batch_loss = positive_loss + negative_loss
+        loss = np.sum(batch_loss)
+
+        return loss
+
 if __name__ == "__main__":
-    w2v = Word2Vec(1000, 300)
-    
-    output = w2v.forward(np.array([[2, 3], [1, 2]]))
-    print(output.shape)
+    w2v = SG_Word2Vec(1000, 300)
+
+    center_word = np.array([[2], [5], [3], [2]])
+    positive_context = np.array([[7], [43], [12], [4]])
+    negative_samples = np.array([[5, 3, 2], [5, 6, 7], [4, 3, 2], [9, 8, 1]])
+
+    loss = w2v.forward(center_word, positive_context, negative_samples)
+    print(loss)
